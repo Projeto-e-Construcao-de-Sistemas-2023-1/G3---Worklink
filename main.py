e1a85d1bee341cd5b604fa1a63e5d9c6c51d3967
jonathansouzaa <jonathanrds55@edu.unirio.br>
2023-05-22 22:43:32 -0300

    adicionando arquivos front-end

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Arquivo main para testes de funcionalidades das classes e do BD
 from Desenvolvedor import Desenvolvedor
 from Empresa import Empresa
-
+from login import login
+app =
 def main():
     pass
 if(__name__) == '__main__':
