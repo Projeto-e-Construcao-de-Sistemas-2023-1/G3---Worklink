2cbca976c20d40e764d700f50d6dbcf9b5f19632
jonathansouzaa <90422141+jonathansouzaa@users.noreply.github.com>
2023-05-24 12:06:54 -0300

    Update main.py
    Agora o form html retorna o login e senha digitados pelo usuario ao flask, coloquei o print pra verificar no terminal se as credenciais sao repassadas. o doc está conectado ao html

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 # Arquivo main para testes de funcionalidades das classes e do BD
 from Desenvolvedor import Desenvolvedor
 from Empresa import Empresa
+ from flask import Flask, render_template, redirect, request
 
 def main():
     dev1 = Desenvolvedor()
     dev1.setEmail()
 if(__name__) == '__main__':
     main()
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    print('email: ', email)
+    print('senha', password)
+    return redirect('/')
