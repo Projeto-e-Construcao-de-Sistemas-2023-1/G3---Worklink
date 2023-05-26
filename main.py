40dba521fd026bb5fc5b50fb039ea148029665a7
jonathansouzaa <90422141+jonathansouzaa@users.noreply.github.com>
2023-05-26 13:08:36 -0300

    Update main.py
    adicionando coleta email senha das paginas de dev e emp

--- main.py
+++ main.py
@@ -3,35 +3,52 @@ from Desenvolvedor import Desenvolvedor
 from Empresa import Empresa
 from flask import Flask, render_template, redirect, request, abort
 from Usuario import Usuario
-import requests 
+
 emp = Empresa()
 dev = Desenvolvedor()
 dev.conectaBD()
-dev.criaDesenvolvedor('dev@email.com', '21989212222', '123456789', '12345', 'desenvolvedor', 'pleno', 'nb', 'java', '12345678900', '12/12/1995')
+dev.criaDesenvolvedor('dev@email.com', '21989212222', '123456789', '12345', 'desenvolvedor', 'pleno', 'nb', 'java',
+                      '12345678900', '12/12/1995')
 app = Flask(__name__)
+
+
 @app.route('/')
-
-
 def home():
-        return render_template('login.html', site_key=SITE_KEY)
+    return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
-def login():
+
+@app.route('/loginDesenvolvedor', methods=['POST'])
+def logindev():
     email = request.form.get('email')
     password = request.form.get('password')
-    # SE FOR INICIAR SESSAO COMO DESENVOLVEDOR
-    dev.iniciaSessao(email, password)
-    # SE FOR INICIAR SESSAO COMO EMPRESA
-    emp.iniciaSessao(email, password)
     if dev.iniciaSessao(email, password) == True:
+        return render_template('feed.html')
         # Avançar de página
         print('Passei!')
         pass
     else:
         print('Erro')
+        return render_template('index.html')  # criar pagina de erro com para nova tentativa
         # Printar erro
         pass
-    return redirect('/')
+
+
+@app.route('/loginempresa', methods=['POST'])
+def loginemp():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if emp.iniciaSessao(email, password) == True:
+        return render_template('feed.html')
+        # Avançar de página
+        print('Passei!')
+        pass
+    else:
+        print('Erro')
+        return render_template('index.html')  # criar pagina de erro com para nova tentativa
+        # Printar erro
+        pass
+    return redirect('/')  # aqui o final do antigo cofigo
+
 
 @app.route("/sign-user-in", methods=['POST'])
 def sign_in_user():
@@ -45,12 +62,10 @@ def sign_in_user():
         abort(401)
 
     return redirect(url_for('home'))
-                       
 
     return redirect(url_for('home'))
 
 
-SITE_KEY = '6LeKBj8mAAAAAA3jCMVID2PjUUYmIM1TYOIKf3Ei'
-SECRET_KEY ='6LeKBj8mAAAAAAIsJpHljREaS2EPF8y5uw2frJHA'
-VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify'
-
+SITE_KEY = "6LeKBj8mAAAAAA3jCMVID2PjUUYmIM1TYOIKf3Ei"
+SECRET_KEY: = "6LeKBj8mAAAAAAIsJpHljREaS2EPF8y5uw2frJHA"
+VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify"
