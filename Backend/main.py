989a47be24693ea866d93ef9b8b3288034cca933
Rafael <rafael.kawakubo@usereserva.com>
2023-06-05 19:54:30 -0300

    configuracoes

--- Backend/main.py
+++ Backend/main.py
@@ -3,12 +3,14 @@ from Desenvolvedor import Desenvolvedor
 from Empresa import Empresa
 from flask import Flask, render_template, redirect, request, requests, abort, url_for
 
+app = Flask(__name__, template_folder="templates")
+
 emp = Empresa()
 dev = Desenvolvedor()
 dev.conectaBD()
 dev.criaDesenvolvedor('dev@email.com', '21989212222', '123456789', '12345', 'desenvolvedor', 'pleno', 'nb', 'java',
                       '12345678900', '12/12/1995')
-app = Flask(__name__)
+
 
 @app.route('/')
 def home():
@@ -68,6 +70,10 @@ def sign_in_user():
     return redirect(url_for('home'))
 
     return redirect(url_for('home'))
+
+if __name__ == "__main__":
+    app.run()
+
 
 
 SITE_KEY = "6LeKBj8mAAAAAA3jCMVID2PjUUYmIM1TYOIKf3Ei"
