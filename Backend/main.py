14d4deb67f63ac9c6d4dbf83aa0be86dd29b1ae2
jonathansouzaa <jonathanrds55@edu.unirio.br>
2023-06-09 20:09:10 -0300

    alteração html para outras paginas

--- Backend/main.py
+++ Backend/main.py
@@ -1,31 +1,57 @@
 # Arquivo main para testes de funcionalidades das classes e do BD
-from Desenvolvedor import Desenvolvedor
-from Empresa import Empresa
-from Database import Database
+#from Desenvolvedor import Desenvolvedor
+#from Empresa import Empresa
+#from Database import Database
 from flask import Flask, render_template, redirect, request, abort, url_for
 import requests
 from datetime import datetime as dt
 
 app = Flask(__name__, template_folder="templates")
 
-emp = Empresa()
-dev = Desenvolvedor()
-dev.criaDesenvolvedor('desenvolvedor', 'senior', '19828347589', 'dev@outlook.com', 'masculino', '2000/12/12', '(21)8573487509', '12345678901',
-                      'senha', 'pleno', 'gigantesca', 'python')
+#emp = Empresa()
+#dev = Desenvolvedor()
+#dev.criaDesenvolvedor('desenvolvedor', 'senior', '19828347589', 'dev@outlook.com', 'masculino', '2000/12/12', '(21)8573487509', '12345678901',
+#                     'senha', 'pleno', 'gigantesca', 'python')
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/loginDesenvolvedor', methods=['POST'])
-def logindev():
-    secret_response = request.form['g-recaptcha-response']
-    verify_response = requests.post(url=f'{VERIFY_URL}?secret={SECRET_KEY}&response={secret_response}')
-    print(verify_response)
+@app.route('/authlogin', methods=['GET'])
+def authlog():
+    email = request.form.get('e-mail')
+    password = request.form.get('password')
+    print(email)
+    print(password)
+    #if dev.iniciaSessao(email, password) == True:
+        #return render_template('feed.html')
+        # Avançar de página
+        #print('Passei!')
+        #pass
+    #else:
+        #print('Erro')
+        #return render_template('index.html')  # criar pagina de erro com para nova tentativa
+        # Printar erro
+        #pass
 
+@app.route('/login', methods=['GET'])
+def login():
+   return render_template('login.html')
 
-@app.route('/loginempresa', methods=['POST'])
+@app.route('/teste', methods=['GET'])
+def teste():
+   return render_template('loginEmpresa.html')
+
+@app.route('/signup_developer', methods=['GET'])
+def regdev():
+   return render_template('RegisterDesenvolvedor.html')
+  
+@app.route('/signup_enterprise', methods=['GET'])
+def regEmp():
+   return render_template('RegisterEmpresa.html')
+
+@app.route('/loginEmpresa', methods=['POST'])
 def loginemp():
     email = request.form.get('email')
     password = request.form.get('password')
@@ -39,7 +65,7 @@ def loginemp():
         return render_template('index.html')  # criar pagina de erro com para nova tentativa
         # Printar erro
         pass
-    return redirect('/')  # aqui o final do antigo codigo
+    return redirect('/')  # aqui o final do antigo cofigo
 
 
 @app.route("/sign-user-in", methods=['POST'])
@@ -57,10 +83,10 @@ def sign_in_user():
 
     return redirect(url_for('home'))
 
+
 if __name__ == "__main__":
     app.run()
 
-
-# SITE_KEY = "6LeKBj8mAAAAAA3jCMVID2PjUUYmIM1TYOIKf3Ei"
-# SECRET_KEY = "6LeKBj8mAAAAAAIsJpHljREaS2EPF8y5uw2frJHA"
-# VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify"
+SITE_KEY = "6LeKBj8mAAAAAA3jCMVID2PjUUYmIM1TYOIKf3Ei"
+SECRET_KEY = "6LeKBj8mAAAAAAIsJpHljREaS2EPF8y5uw2frJHA"
+VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify"
