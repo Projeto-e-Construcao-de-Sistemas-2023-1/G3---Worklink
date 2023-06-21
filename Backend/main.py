0dccae8850019260b31bd9850a5001fdb38aa47c
jonathansouzaa <jonathanrds55@edu.unirio.br>
2023-06-20 22:32:43 -0300

    com as edições do rafael. Lgn funfand com recapcha

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,8 @@
-# Arquivo main para testes de funcionalidades das classes e do BD
-from Desenvolvedor import Desenvolvedor
+from flask import Flask, render_template, request, redirect, url_for, abort, jsonify, flash
 from Empresa import Empresa
 from Database import Database
 from Usuario import Usuario
-from flask import Flask, render_template, redirect, request, abort, url_for 
+from Desenvolvedor import Desenvolvedor
 from flask_wtf import FlaskForm, RecaptchaField
 import requests
 from datetime import datetime as dt
@@ -37,25 +36,30 @@ def regdv():
 
 @app.route('/signup_emp', methods=['GET'])
 def regem():
-    return render_template('RegisterEmpresa.html') 
+    return render_template('RegisterEmpresa.html')
 
 @app.route('/authlogin', methods=['GET', 'POST'])
 def login():
+    form = Widgets()
     if request.method == 'GET':
-        form = Widgets()
         return render_template('login.html', form=form)
     else:
         email = request.form.get('email')
         password = request.form.get('password')
-        print(email)
-        if dev.iniciaSessao(email, password) == True:
-            #funcao pesquisar email na tabela de dev
+        if not email or not password:
+            flash('Por favor, preencha todos os campos.')
+            return redirect(url_for('login'))
+        
+        elif 'g-recaptcha-response' not in request.form:
+            flash('Por favor, marque a caixa de verificação reCaptcha.')
+            return redirect(url_for('login'))
+        
+        elif dev.iniciaSessao(email, password):
             dev.capturaEmail(email)
-            form = Widgets()
-            return render_template('pagina_inicial.html', nome=dev.getNome(), sobrenome=dev.getSobrenome(), descricao=dev.getDescricao())
+            return redirect(url_for('feed'))
         else:
-            print('Erro')
-            return render_template('home.html')  # criar pagina de erro com para nova tentativa  
+            flash('Email ou senha incorretos.')
+            return redirect(url_for('login'))
 
 @app.route('/perfil', methods=['GET'])
 def perfildev():
@@ -65,38 +69,16 @@ def perfildev():
 def criar_projeto():
     return render_template('criarProjeto.html')
 
-@app.route('/paginainicial', methods=['GET'])
-def paginainicial():
-    return render_template('pagina_inicial.html')
+@app.route('/feed', methods=['GET'])
+def feed():
+    return render_template('pagina_inicial.html', nome=dev.getNome(), sobrenome=dev.getSobrenome(), descricao=dev.getDescricao())
 #metodos 
-
-# @app.route('/authlogin', methods=['POST', 'GET'])
-# def authlogin():
-#    if request.method == 'POST':
-#         email = request.form.get('email')
-#         password = request.form.get('password')
-#         print(email)
-#         if dev.iniciaSessao(email, password) == True:
-#             #funcao pesquisar email na tabela de dev
-#             dev.capturaEmail(email)
-#             form = Widgets()
-#             return render_template('pagina_inicial.html', nome=dev.getNome(), sobrenome=dev.getSobrenome(), descricao=dev.getDescricao())
-#         else:
-#             print('Erro')
-#             return render_template('home.html')  # criar pagina de erro com para nova tentativa
-#     else:
-#         return render_template('home.html')
     
 @app.route('/pesquisa_usuario', methods=['post'])
 def pesquisaUser():
     pesquisa_user = request.form.get('pesquisa_user')
     print(pesquisa_user)
     
-    # if us.verificaUsuario(pesquisa_user) == True:
-    #     return render_template('resultado_pesquisa.html', nome=dev.getNome(pesquisa_user), descricao=dev.getDescricao(pesquisa_user))
-    # #else:
-    # #   return render_template('resultado_pesquisa.html', nome=emp.getRazaoSocial(pesquisa_user), descricao=emp.getAreaNegocio(pesquisa_user))
-
 @app.route('/signup_developer', methods=['POST'])
 def regdev():
         nome = request.form.get('nome')
@@ -150,6 +132,25 @@ def criarProjeto():
     tag = request.form.get('tag')
     descricao = request.form.get('descricao')
     return render_template('perfil_dev.html')
+  
+@app.route('/follow', methods=['POST'])
+def follow():
+    Id = request.json['Id']
+    Usuario.Follow(Id)
+    return jsonify(success=True)
+
+@app.route('/unfollow', methods=['POST'])
+def unfollow():
+    Id = request.json['Id']
+    Usuario.Unfollow(Id)
+    return jsonify(success=True)
+
+@app.route("/calendario", methods=["GET", "POST"])
+def index():
+  return render_template("S4A_calendar.html")
+
 
 if __name__ == "__main__":
     app.run()
+
+VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify"
