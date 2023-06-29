51e65d16776f4e246befd0c449b711ba72828ebd
Júlia Tonieto <juxtonie@hotmail.com>
2023-06-29 14:00:50 -0300

    botão seguir

--- Backend/static/script.js
+++ Backend/static/script.js
@@ -5,3 +5,16 @@ select.addEventListener('click', function() {
    post.classList.toggle("hide");
 
 });
+
+function alterarTexto() {
+   var btn = document.getElementById("follow-button");
+   if (btn.innerHTML === "Seguir") {
+     btn.innerHTML = "Seguindo";
+     btn.classList.remove("seguir");
+     btn.classList.add("seguindo");
+   } else {
+     btn.innerHTML = "Seguir";
+     btn.classList.remove("seguindo");
+     btn.classList.add("seguir");
+   }
+ }
